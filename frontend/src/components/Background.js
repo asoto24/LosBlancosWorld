ca1e56c32793cbcc7b62020ba3b016f9564e85ae
Daseats96 <lex2703.st@gmail.com>
2023-07-13 17:58:00 -0400

    background image displays properly on the home page, including the animation to make it appear slowly.

--- frontend/src/components/Background.js
+++ frontend/src/components/Background.js
@@ -1,5 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import logo from '../assets/madrid_logo.png';
+import './background.css';
+
+
+
 const Background = () => {
-    return (<div>
-        
-    </div>)
-}
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const image = new Image();
+        image.src = logo;
+        image.onload = () => {
+            setIsLoaded(true);
+        };
+    }, []);
+
+    return (
+        <div className={`image-container ${isLoaded ? 'loaded' : ''}`}>
+            <img alt="Logo" src={logo} />
+        </div>
+    );
+};
+
+export default Background;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Background from "../components/Background";
 
 const Home = () => {
-    return (
-        <Navbar/>
-
+    return (<>
+            <Navbar/>
+            <Background/>
+        </>
     )
 }
 
