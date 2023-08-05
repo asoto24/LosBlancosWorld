a45555418b5a822cd56354aee5e610402965d456
Daseats96 <lex2703.st@gmail.com>
2023-08-05 16:28:19 -0400

    new files

---
+++ backend/src/test/java/backend/LosBlancosWorld/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.LosBlancosWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
